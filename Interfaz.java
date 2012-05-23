7b2b38fae95d2323cf981d6a1de49e259fa0ca51
Giuseppe De Simone <giuseppe@Mjolnir.(none)>
2012-05-23 15:49:19 -0430

    Ya se guardan los procesos ordenados en un arreglo

--- Interfaz.java
+++ Interfaz.java
@@ -7,6 +7,8 @@ import javax.xml.parsers.DocumentBuilder;
 public class Interfaz {
 
     // aqui deberia ir donde se van a guardar los procesos
+    static int nprocesos = -1;
+    static Proceso [] est_procesos; 
 
     private static boolean esNumero(String cadena){
 	try {
@@ -17,6 +19,29 @@ public class Interfaz {
 	}
     }
 
+    public static void ordenarProc(Proceso proce,int pos,int nprocesos) {
+	boolean cambio = false;
+    
+	for(int j=0; j<=pos;j++) {
+	    Proceso iterador = est_procesos[j];
+	    	    
+	    if(iterador.getTiempo_llegada() >= proce.getTiempo_llegada()) {
+		
+		Proceso aux = est_procesos[j];
+		est_procesos[j] = proce;
+		
+		for(int k=j+1; k<=pos; k++) {
+		    Proceso aux2 = est_procesos[k];
+		    est_procesos[k] = aux;
+		    aux = aux2;
+		}
+		j=pos;
+		cambio = true; 		
+	    }
+		
+	}
+    }
+    
     public static LinkedList<Integer> cargarRafagas(NodeList rafagas){
 
 	LinkedList<Integer> rafagasL = new LinkedList<Integer>();
@@ -27,10 +52,16 @@ public class Interfaz {
 		Element cpu = (Element) cpu_nodo;
 		String cpuR = cpu.getAttribute("ciclos");
 		if (esNumero(cpuR)) {
-		    rafagasL.add(Integer.parseInt(cpuR));
+		    int raf = Integer.parseInt(cpuR);
+		    if(raf>0) {
+			rafagasL.add(raf);
+		    } else {
+			System.out.println("Las rafagas de CPU deben ser enteros > 0");
+			System.exit(1);
+		    }
 		} else {
 		    System.out.println("Las rafagas de CPU deben ser enteros > 0");
-		    
+		    System.exit(1);
 		}
 	    }
 	}
@@ -43,6 +74,8 @@ public class Interfaz {
 	
 	int t_llegada = -1;
 	int id = 1;
+	nprocesos = listaProc.getLength();
+	est_procesos = new Proceso[nprocesos];
 		
 	// Recorrer los procesos 
 	for(int i=0; i<listaProc.getLength(); i++){
@@ -56,8 +89,13 @@ public class Interfaz {
 		// Guarda el tiempo llegada en t_llegada
 		if (esNumero(llegada)) {
 		    t_llegada = Integer.parseInt(llegada);
+		    if(t_llegada < 0) {
+			System.out.println("El tiempo de llegada debe ser entero > 0");
+			System.exit(1);
+		    }
 		} else {
-		    System.out.println("El tiempo de llegada debe ser entero >= 0");
+		    System.out.println("El tiempo de llegada debe ser entero > 0");
+		    System.exit(1);
 		}
 		
 		NodeList rafagas= proc.getChildNodes();
@@ -71,15 +109,23 @@ public class Interfaz {
 		proc_e.setRafagas_cpu(rafagasL);
 		proc_e.setPrioridad(0);
 
+		if( i==0 ) {
+		    est_procesos[0] = proc_e;
+		} else {
+		    est_procesos[i] = proc_e;
+		    ordenarProc(proc_e,i,nprocesos);
+		}
+		
 		/* Aqui estoy immprimiendo los datos de cada proceso, no lo borre por si acaso lo 
 		   llegan a necesitar, sino pueden borrarlo tranquilamente
-		*/	
+			
+		System.out.println("# de procesos: "+nprocesos);
 		System.out.println("El tiempo llegada es "+proc_e.getTiempo_llegada());
 		for(int ii=0; ii<proc_e.getRafagas_cpu().size();ii++) {
 		    System.out.println(proc_e.getRafagas_cpu().get(ii));
 
 		 }
-		
+		*/
 
 
 		// aqui deberia guardarse el proc
@@ -101,6 +147,16 @@ public class Interfaz {
 	
 	    cargarProcesos(listaProc);
 	
+	     System.out.println("# de procesos: "+nprocesos);
+	
+	    for(int ii=0; ii<nprocesos;ii++) {
+		System.out.println("Proceso "+ii);
+		System.out.println("    Tiempo llegada: "+est_procesos[ii].getTiempo_llegada());
+
+
+		}
+	    
+
 	} catch (Exception e) {
 	    //System.err.println("Error al abrir el archivo");
 	    e.printStackTrace();
