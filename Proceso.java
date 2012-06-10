a47f94f78d75159354816e3eac9298faf9ec8d43
Sofia Bravo <sofiabravo103@gmail.com>
2012-06-10 16:30:25 -0430

    getRafaga

--- Proceso.java
+++ Proceso.java
@@ -105,7 +105,13 @@ public class Proceso {
     /*Precondicion: No es la ultima rafaga*/
     public int getRafaga(){
 	if(this.rafaga==0){
-	    //quitar la primera rafaga, setear this.rafaga al entero q dio
+	    Integer next = rafagas_cpu.pollFirst();
+	    if(next == null){
+		//Se terminaron las rafagas!
+	    }
+	    else {
+		this.rafaga = next;
+	    }
 	}
 	return this.rafaga;
     }
